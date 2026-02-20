06f80bf3ee85c0ac96ea076e70c74a9feb6424e5
think1 <think1@localhost.localdomain>
2026-02-20 11:27:16 -0500

    good

---
+++ index.js
@@ -0,0 +1,16 @@
+/*
+Copyright 2016 Certified CoderZ
+Author: Brandon Poole Sr. (biz9framework@gmail.com)
+License GNU General Public License v3.0
+Description: BiZ9 Framework: User
+*/
+const {Scriptz}=require("biz9-scriptz");
+const {Log}=require("biz9-utility");
+const {Type}=require("biz9-logic");
+class Primary {
+    static get = async () => {
+    }
+}
+module.exports = {
+    Primary
+};
